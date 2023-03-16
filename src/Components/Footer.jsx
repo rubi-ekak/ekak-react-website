49b68fc2c80d2eb9be5c77ef50b5e172722c531f
rubi-ekak <125873059+rubi-ekak@users.noreply.github.com>
2023-03-16 13:46:06 +0530

    Add files via upload
    UI changes 16-03-2023 13:45

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Footer.css";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import CopyrightOutlinedIcon from "@mui/icons-material/CopyrightOutlined";
 
 const Footer = () => {
   return (
@@ -13,13 +13,11 @@ const Footer = () => {
           <div className="footer-links">
             <ul>
               <a href="#" target="_blank">
-                {" "}
                 <li>
                   <span className="textlink">A</span>
                 </li>
               </a>
               <a href="#" target="_blank">
-                {" "}
                 <li>
                   <span className="textlink">B</span>
                 </li>
@@ -27,13 +25,11 @@ const Footer = () => {
             </ul>
             <ul>
               <a href="#" target="_blank">
-                {" "}
                 <li>
                   <span className="textlink">A</span>
                 </li>
               </a>
               <a href="#" target="_blank">
-                {" "}
                 <li>
                   <span className="textlink">B</span>
                 </li>
@@ -41,59 +37,70 @@ const Footer = () => {
             </ul>
             <ul>
               <a href="#" target="_blank">
-                {" "}
                 <li>
                   <span className="textlink">A</span>
                 </li>
               </a>
               <a href="#" target="_blank">
-                {" "}
                 <li>
                   <span className="textlink">B</span>
                 </li>
               </a>
             </ul>
             <ul>
-              
-                
-                <li>
+              <li>
                 <button type="button" className="ftrbtn">
                   <span className="textbtn bg-white">A</span>
-                  </button>
-                </li>
-              
-             
-                
-                <li>
+                </button>
+              </li>
+
+              <li>
                 <button type="button" className="ftrbtn">
                   <span className="textbtn bg-white">B</span>
-                  </button>
-                </li>
-            
+                </button>
+              </li>
             </ul>
             <ul>
-              
-                
-                <li>
+              <li>
                 <button type="button" className="ftrbtn">
                   <span className="textbtn bg-white">A</span>
-                  </button>
-                </li>
-                </ul>
+                </button>
+              </li>
+            </ul>
           </div>
 
           <div className="footer-content  align-items-center justify-centent-center ">
-             <div className="footer-contacts d-flex align-items-center justify-content-evenly pt-4">
-                 <div className="d-flex align-items-center justify-content-center"><img src="https://ekak.in/assets/images/Ekak-12.png" className="img-fluid logosmall" alt="ekakimages">
-                 </img></div>
-                 <div className="d-flex align-items-center "><HomeOutlinedIcon className="home"/><span className="ml-4">113/216-A Swaroop Nagar, Kanpur- 208002,Uttar Pradesh, India</span></div>
-                 <div className="d-flex align-items-center "><CallOutlinedIcon  className="home"/><span>+91 0512 402 8469</span> </div>
-                 <div className="d-flex align-items-center "><EmailOutlinedIcon  className="home"/> <span>contact@ekak.in </span></div>
-             </div>
+            <div className="footer-contacts d-flex align-items-center justify-content-evenly pt-4">
+              <div className="d-flex align-items-center justify-content-center">
+                <img
+                  src="https://ekak.in/assets/images/Ekak-12.png"
+                  className="img-fluid logosmall"
+                  alt="ekakimages"
+                ></img>
+              </div>
+              <div className="d-flex align-items-center ">
+                <HomeOutlinedIcon className="home" />
+                <span className="ml-4">
+                  113/216-A Swaroop Nagar, Kanpur- 208002,Uttar Pradesh, India
+                </span>
+              </div>
+              <div className="d-flex align-items-center ">
+                <CallOutlinedIcon className="home" />
+                <span>+91 0512 402 8469</span>{" "}
+              </div>
+              <div className="d-flex align-items-center ">
+                <EmailOutlinedIcon className="home" />{" "}
+                <span>contact@ekak.in </span>
+              </div>
+            </div>
             <hr className=""></hr>
-             <div className="footer-copyright d-flex  justify-content-center paracopyright">
-             <CopyrightOutlinedIcon className="paracopyright1"/> <p className=""> Copyright Ekak Innovations Private Limited 2021</p>
-             </div>
+            <div className="footer-copyright d-flex  justify-content-center paracopyright">
+              <CopyrightOutlinedIcon className="paracopyright1" />{" "}
+              <p className="">
+                {" "}
+                Copyright Ekak Innovations Private Limited 2021
+              </p>
+            </div>
           </div>
         </div>
       </footer>
