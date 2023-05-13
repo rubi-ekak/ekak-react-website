37afdc82f9467850225e299d49ac1081a447ab9a
rubi-ekak <rubi@ekak.in>
2023-05-13 12:42:15 +0530

    delete file

--- flask_backend/api/ApiHandler.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api, Resource
-
-class ApiHandler(Resource):
-    def get(self):
-        return {
-        'resultStatus': 'SUCCESS',
-        'message': "Hello Api Handler ApiHandler.py ruiewruqiue"
-        }
